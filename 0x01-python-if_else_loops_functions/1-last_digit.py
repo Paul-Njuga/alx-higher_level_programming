8d0730cf488b391dc869cd25786cbfdae5774bea
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-06-01 16:50:24 -0700

    lstDigit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,5 +6,5 @@ if rem > 5:
     print(f"Last digit of {number} is {rem} and is greater than 5")
 elif rem == 0:
     print(f"Last digit of {number} is {rem} and is 0")
-elif rem < 6 && rem != 0:
+elif rem < 6 and rem != 0:
     print(f"Last digit of {number} is {rem} and is less than 6 and not 0")
