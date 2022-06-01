147590f9ec57f323ed919c65bb8b734b1020fe78
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-06-01 16:56:18 -0700

    lstDigit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    rem = ((number * -1) % 10) * -1)
+    rem = (((number * -1) % 10) * -1)
 else:
     rem = number % 10
 if rem > 5:
