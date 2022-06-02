1f2c98f077ff457057de87352e9f615a88bb5c13
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-06-02 15:21:19 -0700

    No of Args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     num = len(sys.argv)
     if num == 1:
-        print("{} arguments.".format(num - 1))
+        print("{} argument:".format(num - 1))
     elif num == 2:
         print("{} arguments:".format(num - 1))
     else:
