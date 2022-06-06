f4995ec51487a17fa876ba015d622af94b3cfcc8
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-06-06 16:56:55 -0700

    Commit task 10 through 12

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     new_list = []
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            new_list.append(True)
+            new_list = [True]
         else:
-            new_list.append(False)
+            new_list = [False]
     return new_list 
