dd0887c48d56647fe6b7684384bb4ea1cd4af0d5
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-06-08 19:47:19 -0700

    task 1 correction

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
+    new_list = []
     for i in range(len(my_list)):
         if new_list[i] == search:
             new_list.append(replace)
