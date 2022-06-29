0c0f75782b9f9272393ae2ae224a1f7e4882068f
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-06-28 18:42:21 -0700

    Everything is object files

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(i=[0]):
-    i[0] += 1
-    return str(("BestSchool, " * (i[0] - 1)) + "BestSchool$")
+def magic_string():
+    setattr(magic_string, 'str', getattr(magic_string, 'str', -1) + 1)
+    return (("BestSchool, " * magic_string.str) + "BestSchool$")
