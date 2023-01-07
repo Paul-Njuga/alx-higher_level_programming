bbe76bea3a573e48355b988e4221d034a27cd5b3
Paul Njuga <paulnjuga@gmail.com>
2023-01-07 03:01:24 +0300

    Reset to refresh

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 number = 3.14159
-f'Float: {number:.2f}\n'
+print(f'Float: {number:.2f}\n')
--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 str = "Holberton School"
-f'{str * 3}\n{str[:9]}'
+print(f'{str * 3}\n{str[:9]}')
