0ed40663b2e335ab01aaa440579d57b7300f880a
Paul Njuga <paulnjuga@gmail.com>
2023-01-09 00:08:47 +0300

    Customize infinite_add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    result = 0
-    for i in range(1, len(sys.argv)):
-        result += int(sys.argv[i])
-    print("{}".format(result))
+    from sys import argv
+    tot = 0
+    for i in range(1, len(argv)):
+        tot += int(argv[i])
+    print(f"{tot}")
