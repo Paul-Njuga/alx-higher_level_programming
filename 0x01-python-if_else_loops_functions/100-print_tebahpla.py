7666733862fcccc89fa51f55ce9fba97b460c2d7
Paul Njuga <paulnjuga@gmail.com>
2023-01-08 04:10:07 +0300

    Shmateo

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for c in range(ord('z'), ord('a') - 1, -1):
-	if c % 2 == 0:
-		print("{:c}".format(c), end="")
-	else:
-		print("{:c}".format(c - 32), end="")
+    if c % 2 == 0:
+        print("{:c}".format(c), end="")
+    else:
+        print("{:c}".format(c - 32), end="")
