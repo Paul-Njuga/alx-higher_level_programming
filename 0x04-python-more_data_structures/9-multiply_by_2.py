1532fc78392504da3c468afd7e9195734f1f228d
Paul Njuga <paulnjuga@gmail.com>
2023-01-14 23:03:30 +0300

    Add best score correction

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    new_d = {}
-    for i in a_dictionary:
-        new_d[i] = a_dictionary[i] * 2
-    return new_d
+    new_dict = a_dictionary.copy()
+    for key in new_dict:
+        new_dict[key] *= 2
+    return new_dict
