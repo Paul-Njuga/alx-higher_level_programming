cec47b1a73d850d05feea14d9bf0fb5b4b427c2b
Paul Njuga <paulnjuga@gmail.com>
2023-01-14 05:12:53 +0300

    Add append to dict

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     if a_dictionary:
-        for k in a_dictionary:
-            if k == key:
-                a_dictionary[k] = value
-            else:
-                a_dictionary[key] = value
+        for key not in a_dictionary:
+            a_dictionary[key] = value
+        else:
+            for k in a_dictionary:
+                if k == key:
+                    a_dictionary[k] = value
     return a_dictionary
