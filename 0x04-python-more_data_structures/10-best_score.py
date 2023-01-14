5648318b8db9295e1adbeb8888a32673a7b50b7b
Paul Njuga <paulnjuga@gmail.com>
2023-01-14 23:15:20 +0300

    Add best score correction

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None or a_dictionary == {}:
+    if a_dictionary is None or a_dictionary == {}:
         return None
-    if max(a_dictionary, key=a_dictionary.get) is 0:
+    if max(a_dictionary, key=a_dictionary.get) == 0:
         return None
     return max(a_dictionary, key=a_dictionary.get)
