e8d9a49dfaea7a5bc11d37f3124418bf84cfbb1d
Paul Njuga <paulnjuga@gmail.com>
2023-01-14 05:22:03 +0300

    Add delete key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key not in a_dictionary:
-        return a_dictionary
-    else:
-        for k in a_dictionary:
-            if k == key:
-                del a_dictionary[key]
+    if key in a_dictionary:
+        del a_dictionary[key]
     return a_dictionary
