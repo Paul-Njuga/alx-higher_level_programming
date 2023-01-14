f7188b61e3e532858083df2d9c356b223b13b9a6
Paul Njuga <paulnjuga@gmail.com>
2023-01-14 23:07:45 +0300

    Add best score correction

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if a_dictionary:
+        return max(a_dictionary)
+    else:
         return 'None'
-    return max(a_dictionary)
