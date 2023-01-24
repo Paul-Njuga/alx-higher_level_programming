6600c0739827790954eb85942a847f2bebd7893e
Paul Njuga <paulnjuga@gmail.com>
2023-01-25 02:23:14 +0300

    Add 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
+"""Defines a square."""
+
 
 class Square:
-    """Defines a square.
+    """Represents a square.
     Private instance attribute: size.
     Instantiation with size (no type/value verification).
     """
