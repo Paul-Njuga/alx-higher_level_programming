8a1c6769002c66b49e3b050a99968292b7ad6c1f
Paul Njuga <paulnjuga@gmail.com>
2023-01-26 23:32:15 +0300

    Picky commentor

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 
-"""Defines a square."""
+"""Defines a Square."""
 
 
 class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with size (no type/value verification).
+    """
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
     """
 
     def __init__(self, size):
-        """Initializes the data."""
+        """
+        Args:
+            private size (int): Size of the square.
+        """
         self.__size = size
