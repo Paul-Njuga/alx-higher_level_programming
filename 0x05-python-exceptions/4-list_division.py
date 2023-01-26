723e37e3f95a5f89cd128716a4ef02d7d8ad031f
Paul Njuga <paulnjuga@gmail.com>
2023-01-26 04:57:43 +0300

    Rmv whitespace

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     div = 0
-    
+
     for i in range(list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
