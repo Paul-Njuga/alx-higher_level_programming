876fe81604378cc4d1b3da740de6fc687c6c1df3
Paul Njuga <paulnjuga@gmail.com>
2023-01-26 19:03:17 +0300

    Return function directly

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,8 +2,7 @@
 def safe_function(fct, *args):
     import sys
     try:
-        ret = fct(*args)
-        return ret
+        return fct(*args)
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return None
