10e8ca662ac8fbd2e138d606f49716da296fdbe7
Paul Njuga <paulnjuga@gmail.com>
2023-01-27 00:57:54 +0300

    Add print_square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-
 """Printing a square: """
 
 
 class Square:
-    """Represents a square.
-    Private instance attribute: size:
-        - property def size(self)
-        - property setter def size(self, value)
+    """
+    Defines a Square.
+    Private instance attribute: size.
+        - property def size(self).
+        - property setter def size(self, value).
     Instantiation with optional size.
     Public instance method: def area(self).
     Public instance method: def my_print(self).
     """
-
     def __init__(self, size=0):
-        """Initializes the data."""
+        """
+        Args:
+            private optional size (int): Size of the square.
+        """
         self.__size = size
 
     @property
     def size(self):
-        """Retrieves the size."""
+        """Size getter method."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets the size to a value."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -36,11 +37,14 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        """Prints to stdout the square with the character #."""
-        if self.__size == 0:
-            print()
+        """
+        Prints in stdout the square with the character #.
+        """
+        size = self.__size
+        if size == 0:
+            print()  #: Newline
         else:
-            for i in range(0, self.__size):
-                for j in range(0, self.__size):
+            for i in range(size):
+                for j in range(size):
                     print("#", end="")
-                print()
+                print()  #: Newline
