ebdab1ee5db63685a8968ed26398a9baa8c14a32
Paul Njuga <paulnjuga@gmail.com>
2023-01-31 02:32:02 +0300

    Add attributes to class

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Simple rectangle: """
+
+
+class Rectangle:
+    """
+    Defines a Rectangle.
+    Private instance attribute: width.
+    Private instance attribute: height.
+    Instantiation with optional width and height.
+    """
+    def __init__(self, width=0, height=0):
+        """Instantializes attributes.
+
+        Arg:
+            width (int, optional): Rectangle width.
+            height (int, optional): Rectangle height.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Returns width."""
+        return self.__width
+
+    @property
+    def height(self):
+        """Returns height."""
+        return self.__height
+
+    @width.setter
+    def width(self, val):
+        if not isinstance(val, int):
+            raise TypeError("width must be an integer")
+        elif val < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            return self.__width
+
+    @height.setter
+    def height(self, val):
+        if not isinstance(val, int):
+            raise TypeError("height must be an integer")
+        elif val < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            return self.__height
