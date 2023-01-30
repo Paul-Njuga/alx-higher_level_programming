ff617e659180b85d950715f44db66df5fceffbdc
Paul Njuga <paulnjuga@gmail.com>
2023-01-31 02:38:42 +0300

    Change val to value

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,33 +16,33 @@ class Rectangle:
             width (int, optional): Rectangle width.
             height (int, optional): Rectangle height.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """Returns width."""
+        """Returns rectangle width."""
         return self.__width
 
     @property
     def height(self):
-        """Returns height."""
+        """Returns rectangle height."""
         return self.__height
 
     @width.setter
-    def width(self, val):
-        if not isinstance(val, int):
+    def width(self, value):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif val < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         else:
             return self.__width
 
     @height.setter
-    def height(self, val):
-        if not isinstance(val, int):
+    def height(self, value):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif val < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         else:
             return self.__height
