cf6ab70560ab2bf04a37db3c5334313ca96925c6
Paul Njuga <paulnjuga@gmail.com>
2023-01-31 03:30:52 +0300

    Add str rep comment

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -22,7 +22,8 @@ class Rectangle:
         self.height = height
 
     def __str__(self):
-        """Print the rectangle with the character #."""
+        """Returns an informal and nicely printable string representation
+        of a Rectangle instance, filled with the '#' character."""
         rec_str = ''
         if self.__width == 0 or self.__height == 0:
             return rec_str
