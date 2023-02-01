08ceb08b18f34dc0627f86e312b098410b006b63
Paul Njuga <paulnjuga@gmail.com>
2023-02-02 01:45:35 +0300

    Edit for pycodestyle

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -12,11 +12,11 @@ def print_square(size):
     """
 
     if (not isinstance(size, int) or (isinstance(size, float) and size < 0)):
-            raise TypeError("size must be an integer")
+        raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
     for i in range(size):
         for j in range(size):
             print("#", end="")
         else:
-            print() #: Newline
+            print()  #: Newline
