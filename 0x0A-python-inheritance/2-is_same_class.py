ffcfdb5fcde3b912e32f22997bad67808b85ba14
Paul Njuga <paulnjuga@gmail.com>
2023-02-07 02:25:47 +0300

    Add same class module

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Exact same object: """
+
+
+def is_same_class(obj, a_class):
+    """Function to determine if obj is an instance of a_class.
+
+    Args:
+        obj: object to look at
+        a_class: class to verify the instance of
+
+    Returns:
+        True if obj is an instance of a_class,
+        False otherwise
+    """
+
+    return True if type(obj) is a_class else False
