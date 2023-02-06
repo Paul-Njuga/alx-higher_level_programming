9b4d3461adc49570ec6f7e9ac0627e11c1764cfa
Paul Njuga <paulnjuga@gmail.com>
2023-02-07 01:40:12 +0300

    Add lookup module

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Lookup: """
+
+
+def lookup(obj):
+    """list available attributes and methods of an object.
+    Arg:
+        obj (:obj:`list`).
+    """
+    return dir(obj)
