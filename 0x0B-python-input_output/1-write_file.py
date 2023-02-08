910a2c2672f2e980ff02790a3f5ff2ced6e84efe
Paul Njuga <paulnjuga@gmail.com>
2023-02-09 02:24:26 +0300

    Add write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Write to a file: """
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8).
+    Returns:
+        Number of characters written.
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
