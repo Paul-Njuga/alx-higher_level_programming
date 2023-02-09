2bf88baab8cfcd3e27e79bc752c84bcaac57edc5
Paul Njuga <paulnjuga@gmail.com>
2023-02-09 04:07:02 +0300

    Add sorting to json dump

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,5 +4,10 @@ import json
 
 
 def to_json_string(my_obj):
-    """Returns the JSON representation of an object (string)."""
-    return json.dumps(my_obj)
+    """Convert an object to a JSON string.
+    Args:
+        my_obj (:obj): Object to convert.
+    Returns:
+        The JSON representation of an object (string).
+    """
+    return json.dumps(my_obj, sort_keys=True)
