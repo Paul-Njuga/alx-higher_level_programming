e147a7e020a13d37273c5c9c9514110f2ba03c87
Paul Njuga <paulnjuga@gmail.com>
2023-02-12 23:23:57 +0300

    Correct for PEP8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,7 +3,7 @@
 
 
 def class_to_json(obj):
-    """Returns the dictionary description with simple data structure 
+    """Returns the dictionary description with simple data structure
     for JSON serialization of an object.
     Args:
         obj (obj): An instance of a Class.
