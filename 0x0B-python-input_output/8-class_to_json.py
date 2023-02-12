b2b3403e69bbf87454520c54fc440a3fe626d69d
Paul Njuga <paulnjuga@gmail.com>
2023-02-12 23:21:59 +0300

    dict class rep

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class to JSON: """
+
+
+def class_to_json(obj):
+    """Returns the dictionary description with simple data structure 
+    for JSON serialization of an object.
+    Args:
+        obj (obj): An instance of a Class.
+    """
+    return obj.__dict__
