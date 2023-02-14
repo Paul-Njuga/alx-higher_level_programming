0e65e4e48e9bdf81ec9450666741ed4e2320318f
Paul Njuga <paulnjuga@gmail.com>
2023-02-14 04:58:49 +0300

    Rmv excess return in frm_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -59,8 +59,9 @@ class Base:
         """
         my_list = []
         if json_string is not None or json_string != "":
+            if type(json_string) != str:
+                raise TypeError("json_string must be a string")
             my_list = json.loads(json_string)
-            return my_list
         return my_list
 
     def create(cls, **dictionary):
