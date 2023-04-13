0d1bbb0991b4c668f66417772c2922bf24de6da7
Paul Njuga <paulnjuga@gmail.com>
2023-04-13 12:11:48 +0300

    Semistandard fixes

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,12 +2,13 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-	let max = process.argv[2];
-	for (let i = 2; i < process.argv.length; i++) {
-		if (process.argv[i] > max) {
-          scd_max = max;
-	      max = process.argv[i];
-		}
-	}
-	console.log(scd_max);
+  let max = process.argv[2];
+  let scdMax;
+  for (let i = 2; i < process.argv.length; i++) {
+    if (process.argv[i] > max) {
+      scdMax = max;
+      max = process.argv[i];
+    }
+  }
+  console.log(scdMax);
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,6 +4,6 @@ if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
 } else {
   const x = Number(process.argv[2]);
   for (let i = 0; i < x; i++) {
-	console.log('X'.repeat(x));
+    console.log('X'.repeat(x));
   }
 }
