889f6290fdea7f979d0a91550c831d9b41f8beec
Paul Njuga <paulnjuga@gmail.com>
2023-04-13 22:50:40 +0300

    Add second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
-  console.log(0);
+  console.log('0');
 } else {
-  let max = process.argv[2];
-  let scdMax;
-  for (let i = 2; i < process.argv.length; i++) {
-    if (process.argv[i] > max) {
-      scdMax = max;
-      max = process.argv[i];
-    }
-  }
-  console.log(scdMax);
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
