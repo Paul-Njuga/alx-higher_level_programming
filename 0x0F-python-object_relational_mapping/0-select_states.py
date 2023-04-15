3246e5fdd4916035914362dd8880c2df25fcfa08
Paul Njuga <paulnjuga@gmail.com>
2023-04-16 02:35:29 +0300

    Add readme, & select state files

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
