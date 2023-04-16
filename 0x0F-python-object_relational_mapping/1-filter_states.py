a072a3b0e63ef6517d2431f591950fc830c88b12
Paul Njuga <paulnjuga@gmail.com>
2023-04-16 20:02:01 +0300

    Indenting for PEP8

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Module that lists all states from the database hbtn_0e_0_usa"""
+"""Module that lists all states with a name\
+    starting with N (upper N) from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
 
@@ -12,7 +13,10 @@ if __name__ == '__main__':
         db=argv[3], charset="utf8")
 
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute(
+        "SELECT * FROM states\
+        WHERE name LIKE 'N%'\
+        ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
