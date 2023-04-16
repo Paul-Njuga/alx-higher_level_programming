c315d323c16baeafa211b95b2c8f8da09d349dc9
Paul Njuga <paulnjuga@gmail.com>
2023-04-16 20:19:21 +0300

    Add my state filter

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module that lists all states with a name\
+    starting with N (upper N) from the database hbtn_0e_0_usa"""
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    from sys import argv
+
+    conn = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=argv[1], passwd=argv[2],
+        db=argv[3], charset="utf8")
+
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states\
+        WHERE name LIKE {}\
+        ORDER BY states.id ASC".format(argv[4]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
