4104ef2ed481b27b72d609a74b2c08adcffdfa78
Paul Njuga <paulnjuga@gmail.com>
2023-04-17 16:03:56 +0300

    Add binary/cast to regex

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     cur = conn.cursor()
     cur.execute(
         "SELECT * FROM states\
-        WHERE name LIKE 'N%'\
+        WHERE name LIKE BINARY 'N%'\
         ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     cur = conn.cursor()
     cur.execute(
         "SELECT * FROM states\
-        WHERE name LIKE '{}'\
+        WHERE name LIKE BINARY '{}'\
         ORDER BY states.id ASC".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
