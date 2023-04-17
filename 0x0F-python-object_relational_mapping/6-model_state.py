f5d3bad19a96e792f48851b465e2f2a07504bc7a
Paul Njuga <paulnjuga@gmail.com>
2023-04-18 01:32:42 +0300

    Rmv port frm engine

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -5,6 +5,6 @@ from model_state import Base, State
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
                 argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
