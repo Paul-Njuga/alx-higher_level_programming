c81e8732e6187794989330e8a46755667d74f79b
Paul Njuga <paulnjuga@gmail.com>
2023-04-18 04:31:28 +0300

    Commit changes first

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     session = Session(engine)
     new_state = State(name='Louisiana')
     session.add(new_state)
+    session.commit()
     state = session.query(State).filter_by(name='Louisiana').first()
     if state:
         print("{}".format(state.id))
