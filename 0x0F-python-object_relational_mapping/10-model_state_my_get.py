3fc7f5567fbc9f8dfbcbf1c8fae63a71b46092f1
Paul Njuga <paulnjuga@gmail.com>
2023-04-18 04:07:10 +0300

    Add injection safe get

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""prints the State object with the name passed as argument,
+from the database hbtn_0e_6_usa"""
+from sys import argv
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    for state in session.query(State).filter(State.name == argv[4]).first():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
