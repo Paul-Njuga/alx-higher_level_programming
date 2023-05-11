e56083aab7920e13e3754aa66bce97f23cd11b1f
Paul Njuga <paulnjuga@gmail.com>
2023-05-11 04:20:25 +0300

    Order by desc

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,4 @@
 -- lists the number of records with the same score in table second_table
 SELECT `score`, count(*) AS 'number' FROM second_table
-GROUP BY score;
+GROUP BY score
+ORDER BY 'number' Desc;
