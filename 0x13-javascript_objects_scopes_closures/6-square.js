22146df8d38a2ecf9cc566cbfe81d27ed5b1eb40
Paul Njuga <paulnjuga@gmail.com>
2023-06-14 04:53:11 +0300

    Revert

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,10 +4,10 @@ const cSquare = require('./5-square.js');
 /* Creates class Square that extends class Square */
 module.exports = class Square extends cSquare {
   charPrint (c) {
-    if (!c) {
+    if (c == null) {
       super.print();
     } else {
-      for (let i = 0; i < this.width; i++) {
+      for (let i = 0; i < this.height; i++) {
         console.log('C'.repeat(this.width));
       }
     }
