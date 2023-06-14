a62765c7787b5a5f50ffc1fb2bd1253b7e42dd77
Paul Njuga <paulnjuga@gmail.com>
2023-06-14 04:41:30 +0300

    Compare C to null

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,7 @@ const cSquare = require('./5-square.js');
 /* Creates class Square that extends class Square */
 module.exports = class Square extends cSquare {
   charPrint (C) {
-    if (!C) {
+    if (C == null) {
       super.print();
     } else {
       for (let i = 0; i < this.height; i++) {
