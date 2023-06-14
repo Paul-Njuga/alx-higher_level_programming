317c8d058a8a4cce6b994041c8f9c1e96e625679
Paul Njuga <paulnjuga@gmail.com>
2023-06-14 04:50:33 +0300

    Uncapitalise C

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,11 +3,11 @@ const cSquare = require('./5-square.js');
 
 /* Creates class Square that extends class Square */
 module.exports = class Square extends cSquare {
-  charPrint (C) {
-    if (C == null) {
+  charPrint (c) {
+    if (!c) {
       super.print();
     } else {
-      for (let i = 0; i < this.height; i++) {
+      for (let i = 0; i < this.width; i++) {
         console.log('C'.repeat(this.width));
       }
     }
