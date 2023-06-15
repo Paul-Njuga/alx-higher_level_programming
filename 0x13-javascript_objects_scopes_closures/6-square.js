3668d3cea7117b07ab429f03d84dd937e08d793f
Paul Njuga <paulnjuga@gmail.com>
2023-06-15 04:55:22 +0300

    Replace string with variable

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,7 +8,7 @@ module.exports = class Square extends cSquare {
       super.print();
     } else {
       for (let i = 0; i < this.height; i++) {
-        console.log('C'.repeat(this.width));
+        console.log(c.repeat(this.width));
       }
     }
   }
