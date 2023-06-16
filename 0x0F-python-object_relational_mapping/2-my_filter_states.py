e5f591dfe1e16a5be30ef5ea158fe42647418fdd
Paul Njuga <paulnjuga@gmail.com>
2023-06-16 17:23:35 +0300

    Fix: Handle importation
    The script should not be imported when imported.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
 """Filter states by user input:
-Takes an argument & displays all values
-in states table of hbtn_0e_0_usa
-where name matches argument"""
+
+Takes an argument & displays all values in states table of hbtn_0e_0_usa
+where name matches argument.
+"""
 import sys
 
 import MySQLdb
 
-args = sys.argv
-conn = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=args[1],
-    passwd=args[2],
-    db=args[3],
-    charset="utf8",
-)
-cur = conn.cursor()
-query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-state_name = args[4]
-# Pass state_name as tuple not a list, bcs it's one value
-cur.execute(query, (state_name,))
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+
+def main():
+    args = sys.argv
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=args[1],
+        passwd=args[2],
+        db=args[3],
+        charset="utf8",
+    )
+    cur = conn.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    state_name = args[4]
+    # Pass state_name as tuple not a list, bcs it's one value
+    cur.execute(query, (state_name,))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
