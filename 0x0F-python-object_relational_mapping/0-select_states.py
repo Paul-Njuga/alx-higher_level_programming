e9c73a887e778e40c6143b5df4bafc000e1ce206
Paul Njuga <paulnjuga@gmail.com>
2023-06-16 18:22:14 +0300

    Fix: Fix for export, arguments, & refactor
    Refactor conn to use 'with' for cleanup. Mv imports to main func. Fix sql query for non-validated arguments.

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,29 +4,27 @@ Get all states:
 
 Lists all states from hbtn_0e_0_usa.
 """
-import sys
-
-import MySQLdb
 
 
 def main():
     """Lists all states"""
-    args = sys.argv
-    conn = MySQLdb.connect(
+    from sys import argv as args
+
+    import MySQLdb
+
+    with MySQLdb.connect(
         host="localhost",
         port=3306,
         user=args[1],
         passwd=args[2],
         db=args[3],
         charset="utf8",
-    )
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    ) as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
 
 
 if __name__ == "__main__":
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,29 +4,32 @@
 Takes an argument & displays all values in states table of hbtn_0e_0_usa
 where name matches argument.
 """
-import sys
-
-import MySQLdb
 
 
 def main():
     """Displays values where name matches state_name"""
-    args = sys.argv
-    conn = MySQLdb.connect(
+    from sys import argv as args
+
+    import MySQLdb
+
+    with MySQLdb.connect(
         host="localhost",
         port=3306,
         user=args[1],
         passwd=args[2],
         db=args[3],
         charset="utf8",
-    )
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = {} ORDER BY id ASC", args[4])
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    ) as conn:
+        cur = conn.cursor()
+        # No argument validation
+        cur.execute(
+            "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(
+                args[4]
+            )
+        )
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
 
 
 if __name__ == "__main__":
