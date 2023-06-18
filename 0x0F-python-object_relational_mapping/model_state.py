413e0a2a6a56f669152e16aa5f8232d33072011d
Paul Njuga <paulnjuga@gmail.com>
2023-06-19 02:32:26 +0300

    Fix: Document class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,8 @@ Base = declarative_base()
 
 
 class State(Base):
+    """Class State"""
+
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
