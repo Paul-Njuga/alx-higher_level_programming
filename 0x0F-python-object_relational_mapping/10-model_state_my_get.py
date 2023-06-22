7b626850e0565cc2e02b925d16c9916e680d62d1
Paul Njuga <paulnjuga@gmail.com>
2023-06-22 23:10:29 +0300

    Fix: Edit for pep8

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     arg = argv[4]
     state = session.query(State).filter_by(name=arg).order_by(State.id).first()
     if state:
