34e999c4a836060118daaac02c012f9d96235b0c
Paul Njuga <paulnjuga@gmail.com>
2023-07-02 20:09:31 +0300

    Rmv arg check

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends request, displays response body size
-if [ "$1" ]; then
-	curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
-fi
+curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
