a9bc88b8fbade304e94984d6cd2f631c2392a80d
Paul Njuga <paulnjuga@gmail.com>
2023-07-26 01:42:43 +0300

    Add statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* display the status code of a GET request */
+const args = process.argv.slice(2);
+
+if (args.length === 1) {
+  const url = args[0];
+  const request = require('request');
+  request(url, function (error, response) {
+    console.log('code:', response.statusCode);
+  });
+} else {
+  console.log('Usage: ./2-statuscode.js URL');
+}
