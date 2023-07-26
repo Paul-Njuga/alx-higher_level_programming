db78037c3af30e0e6e921957412fb0aac7ac8b53
Paul Njuga <paulnjuga@gmail.com>
2023-07-26 05:01:41 +0300

    Add star wars wedge antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+/**
+ * Prints the number of movies where,
+ * the character “Wedge Antilles” is present
+ */
+const args = process.argv.slice(2);
+
+if (args.length === 1) {
+  const url = args[0];
+  let filmNum = 0;
+  const char = 'https://swapi-api.alx-tools.com/api/people/18/';
+  const request = require('request');
+  request(url, function (error, _, body) {
+    if (error) {
+      process.exit(1);
+    }
+    const data = JSON.parse(body);
+    for (const film of data.results) {
+      if (film.characters.includes(char)) {
+        filmNum++;
+      }
+    }
+    console.log(filmNum);
+  });
+} else {
+  console.log('Usage: ./4-starwars_count.js URL');
+}
