95bb2fca161429a15b0ce4238adc21aaaaf9c62e
Paul Njuga <paulnjuga@gmail.com>
2023-08-31 21:11:48 +0300

    Add env for bash & edit comment

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# Takes in a URL, sends request, displays response body size
+#!/bin/env bash
+# Takes in a URL, sends header request, displays response body size
 curl -sI "$1" | grep 'Content-Length' | awk '{print $2}'
