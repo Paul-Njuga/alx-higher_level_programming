35764102f22e6382d2c79f787e257282d66fd813
Paul Njuga <paulnjuga@gmail.com>
2023-11-24 19:01:49 +0300

    Fix: handle ambigous var

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -17,14 +17,14 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    l = [[0 for x in range(i + 1)] for i in range(n)]
-    l[0] = [1]
+    myList = [[0 for x in range(i + 1)] for i in range(n)]
+    myList[0] = [1]
 
     for i in range(1, n):
-        l[i][0] = 1
+        myList[i][0] = 1
         for j in range(1, i + 1):
-            if j < len(l[i - 1]):
-                l[i][j] = l[i - 1][j - 1] + l[i - 1][j]
+            if j < len(myList[i - 1]):
+                myList[i][j] = myList[i - 1][j - 1] + myList[i - 1][j]
             else:
-                l[i][j] = l[i - 1][0]
-    return l
+                myList[i][j] = myList[i - 1][0]
+    return myList
